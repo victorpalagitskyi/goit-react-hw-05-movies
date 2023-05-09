7a0a384af9e1ff0871a7283c40223aa1003e1c28
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-05-09 10:25:18 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import { SharedLayout } from './SharedLayout';
 
 
  
-const Trendings = lazy(() => import('pages/Trending'));
-const Movies = lazy(() => import('pages/Movies'));
-const MovieSpec = lazy(() => import('pages/MovieSpec'));
+const Trendings = lazy(() => import('../pages/Trending'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieSpec = lazy(() => import('../pages/MovieSpec'));
 const Cast = lazy(() => import('./Cast'));
 const Reviews = lazy(() => import('./Reviews'));
 
