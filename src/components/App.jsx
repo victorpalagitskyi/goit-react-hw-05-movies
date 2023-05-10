eb734dae41c49f1274099e721f545f4c265d73c6
Victor20012029103 <112655698+Victor20012029103@users.noreply.github.com>
2023-05-10 23:31:17 +0300

    5565

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -24,6 +24,7 @@ export const App = () => {
             <Route path="cast" element={<Cast />} />
           </Route>
         </Route>
+        <Route path="*" element={<SharedLayout />} />
       </Routes>
     </>
   );
